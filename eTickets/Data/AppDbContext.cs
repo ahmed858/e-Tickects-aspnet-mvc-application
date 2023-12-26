72e8e18e5a567b316fcbc6e9f077a978f2bf32f3
ahmed858 <zez3238@gmail.com>
2023-12-27 00:23:28 +0300

    puse in P14 in course

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+            { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new {
+            am.ActorId,am.MoveiId
+            });
+            modelBuilder.Entity<Actor_Movie>().HasOne(a=>a.Actor).WithMany(am=>am.Actors_Movies).HasForeignKey(a=>a.ActorId);
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m=>m.Movie).WithMany(am=>am.Actors_Movies).HasForeignKey(m=>m.MoveiId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer   > Producers { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+    }
+}
